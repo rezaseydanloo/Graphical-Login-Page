96ff33ec400ecf885dc87bd07731c04f71a3dda1
rezaseydanloo <rezaseydanloo.83@gmail.com>
2024-05-29 00:45:46 +0330

    create for statrt program

---
+++ src/main/java/User/MyApp.java
@@ -0,0 +1,12 @@
+package User;
+
+import GUI.GUISigninClass;
+
+public class MyApp {
+
+    public static void main(String[] args) {
+        GUISigninClass SignIn = new GUISigninClass();
+        SignIn.SignIn();
+
+    }
+}
